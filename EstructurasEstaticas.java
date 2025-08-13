13a7f3c457e3fe53f69934b907d6a417e4d3dd6d
Martin <martincesp8@gmail.com>
2025-08-13 16:25:31 -0500

    segundo

---
+++ EstructurasEstaticas.java
@@ -0,0 +1,8 @@
+public class EstructurasEstaticas {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 11;
+        int s = a+b;
+        System.out.println(s);
+    }
+}
